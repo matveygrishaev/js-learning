51579d17127bb1a2addab9b905a87de61f4933af
matveygrishaev <matveygrishaev@gmail.com>
2020-10-09 21:09:07 +0300

    Практическое задание к уроку №2

--- js/script.js
+++ js/script.js
@@ -1,9 +1,17 @@
-let money;
-let income;
-let addExpenses;
-let deposit;
-let mission;
-let period;
+let money = 90000;
+let income = 'Фриланс';
+let addExpenses = 'Интернет, Такси, Коммуналка';
+let deposit = true;
+let mission = 9000000;
+let period = 6;
+let budgetDay = money / 30;
 
-console.log('Hello');
-alert('Hello');
+console.log(typeof money);
+console.log(typeof income);
+console.log(typeof deposit);
+console.log(addExpenses.length);
+console.log('Период равен ' + period + ' месяцев');
+console.log('Цель заработать ' + mission + ' рублей');
+console.log(addExpenses.toLowerCase());
+console.log(addExpenses.split(', '));
+console.log('budgetDay: ' + budgetDay + ' рублей');
